33feabc1a7f21b324e5aa91ed46edd8720c6b406
lord-solid <mrichard@youverify.co>
2024-08-09 09:07:15 +0100

    -Fixed calculator

--- app/src/androidTest/java/com/solid/materialcalculator/presentation/CalculatorScreenTest.kt
+++ app/src/androidTest/java/com/solid/materialcalculator/presentation/CalculatorScreenTest.kt
@@ -35,6 +35,6 @@ class CalculatorScreenTest {
         composeRule.onNodeWithText("5").performClick()
         composeRule.onNodeWithText("=").performClick()
 
-        composeRule.onNodeWithText("4.0").assertIsDisplayed()
+        composeRule.onNodeWithText("2.0").assertIsDisplayed()
     }
 }
