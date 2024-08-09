9722e3f6dda3da73a9d3b79e6284db072f881284
lord-solid <mrichard@youverify.co>
2024-08-09 08:01:14 +0100

    -Added test for calculator ui

--- app/src/androidTest/java/com/solid/materialcalculator/presentation/CalculatorScreenTest.kt
+++ app/src/androidTest/java/com/solid/materialcalculator/presentation/CalculatorScreenTest.kt
@@ -2,8 +2,6 @@ package com.solid.materialcalculator.presentation
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.solid.materialcalculator.MainActivity
@@ -30,13 +28,13 @@ class CalculatorScreenTest {
     fun enterExpression_correctResultDisplayed(){
         composeRule.onNodeWithText("1").performClick()
         composeRule.onNodeWithText("+").performClick()
-        composeRule.onNodeWithText("2").performClick()
+        composeRule.onNodeWithText("4").performClick()
         composeRule.onNodeWithText("x").performClick()
         composeRule.onNodeWithText("3").performClick()
         composeRule.onNodeWithText("-").performClick()
         composeRule.onNodeWithText("5").performClick()
         composeRule.onNodeWithText("=").performClick()
 
-        composeRule.onNodeWithText("2.0").assertIsDisplayed()
+        composeRule.onNodeWithText("8.0").assertIsDisplayed()
     }
 }
